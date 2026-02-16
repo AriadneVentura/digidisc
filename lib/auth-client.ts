d787b943a1e8d351069ba8b465eb6a10a033f05c
Ariadne <ariadnevk21@gmail.com>
2026-02-16 21:17:26 +1100

    neon db setup

---
+++ lib/auth-client.ts
@@ -0,0 +1,7 @@
+// This is needed for oauth logins like google
+
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient( {
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL!,
+} )
