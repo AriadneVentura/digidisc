36aced16dbdacad36a701f9b680d2ed1280a1144
Ariadne <ariadnevk21@gmail.com>
2026-03-05 22:47:58 +1100

    vercel isnt letting me login, is it nextjs 16

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -2,27 +2,33 @@ import { toNextJsHandler } from "better-auth/next-js";
 import { auth } from "@/lib/auth";
 import { ArcjetDecision, slidingWindow, validateEmail } from "arcjet";
 import aj from "@/lib/arcjet";
-import { NextRequest } from "next/dist/server/web/spec-extension/request";
+import { NextRequest } from "next/server";
 import ip from "@arcjet/ip";
 
 // Email validation
-const emailValidation = aj.withRule( validateEmail( {
-    mode: "LIVE",
-    // disallow spam emails or unverified emails or temp emails
-    deny: [ "DISPOSABLE", "INVALID", "NO_MX_RECORDS" ]
-} ) )
+const emailValidation = aj.withRule(
+    validateEmail( {
+        mode: "LIVE",
+        // disallow spam emails or unverified emails or temp emails
+        deny: [ "DISPOSABLE", "INVALID", "NO_MX_RECORDS" ],
+    } )
+);
 
 // Rate limit sliding window which prevents a burst of requests and then nothing.
-const rateLimit = aj.withRule( slidingWindow( {
-    mode: "LIVE",
-    interval: "2m",
-    // Only allowed to log in twice
-    max: 2,
-    characteristics: [ "fingerprint" ]
-} ) )
+const rateLimit = aj.withRule(
+    slidingWindow( {
+        mode: "LIVE",
+        interval: "2m",
+        // Only allowed to log in twice
+        max: 2,
+        characteristics: [ "fingerprint" ],
+    } )
+);
 
 const protectedAuth = async ( req: NextRequest ): Promise<ArcjetDecision> => {
-    const session = await auth.api.getSession( { headers: req.headers } );
+    const session = await auth.api.getSession( {
+        headers: req.headers,
+    } );
 
     let userId: string;
 
@@ -31,6 +37,13 @@ const protectedAuth = async ( req: NextRequest ): Promise<ArcjetDecision> => {
     } else {
         // default ip that arcjet uses if no other is provided.
         userId = ip( req ) || "127.0.0.1";
+    }
+
+    // Don't protect OAuth callbacks
+    if ( req.nextUrl.pathname.startsWith( "/api/auth/callback" ) ) {
+        return {
+            isDenied: () => false,
+        } as ArcjetDecision;
     }
 
     // User trying to log in
@@ -43,28 +56,25 @@ const protectedAuth = async ( req: NextRequest ): Promise<ArcjetDecision> => {
     }
 
     return rateLimit.protect( req, { fingerprint: userId } );
-}
+};
 
-const authHandlers = toNextJsHandler( auth.handler );
+const authHandlers = toNextJsHandler( auth );
 
-export const { GET } = authHandlers;
+export const GET = authHandlers.GET;
 
 // Secure authentication with email validation, rate limiting and common malicious attacks.
 export const POST = async ( req: NextRequest ) => {
     const decision = await protectedAuth( req );
+
     if ( decision.isDenied() ) {
         if ( decision.reason.isEmail() ) {
             throw new Error( "Email validation failed." );
         }
 
         if ( decision.reason.isRateLimit() ) {
-            throw new Error( "Rate limit exceeded." )
-        }
-
-        if ( decision.reason.isShield() ) {
-            throw new Error( "Shield turned on, protected against malicious actions." )
+            throw new Error( "Rate limit exceeded." );
         }
     }
 
     return authHandlers.POST( req );
-}
+};
