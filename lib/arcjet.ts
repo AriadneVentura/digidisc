96413643c3ebf588b9ceafe6cbfe156a2bfb571b
Ariadne <ariadnevk21@gmail.com>
2026-03-05 20:17:17 +1100

    fix npm build error

--- lib/arcjet.ts
+++ lib/arcjet.ts
@@ -1,5 +1,26 @@
-import arcjet from '@arcjet/next';
-import { getEnv } from "@/lib/utils";
+import arcjet, {
+    ArcjetDecision,
+    createMiddleware,
+    detectBot,
+    fixedWindow,
+    request,
+    shield,
+    slidingWindow,
+    validateEmail,
+} from "@arcjet/next";
+import { getEnv } from "./utils";
+
+// Re-export the rules to simplify imports inside handlers
+export {
+    detectBot,
+    fixedWindow,
+    shield,
+    request,
+    slidingWindow,
+    validateEmail,
+    createMiddleware,
+    ArcjetDecision,
+};
 
 // Secures the digi disc from auth to server actions.
 const aj = arcjet( {
--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,28 @@
-import { NextRequest } from "next/dist/server/web/spec-extension/request";
-import { NextResponse } from "next/dist/server/web/spec-extension/response";
-import { auth } from "@/lib/auth";
-import { detectBot, shield } from "arcjet";
-import aj from "@/lib/arcjet";
-import { createMiddleware } from "@arcjet/next";
+import { NextRequest, NextResponse } from "next/server";
+import aj, { createMiddleware, detectBot, shield } from "./lib/arcjet";
+
+const validate = aj
+    // Shield protects against the most common attacks from the OWASP top 10, before any page is called.
+    .withRule( shield( { mode: "LIVE" } ) )
+    .withRule(
+        detectBot( {
+            mode: "LIVE",
+            allow: [ "CATEGORY:SEARCH_ENGINE", "G00G1E_CRAWLER" ],
+        } )
+    );
 
 // Route protection for Next.js that runs on every request.
-export async function middleware( request: NextRequest, response: NextResponse ) {
-    const session = await auth.api.getSession( {
-        // Check if the user has a session by looking at cookies in headers
-        headers: request.headers,
-    } );
+export default createMiddleware( validate, async ( request: NextRequest ) => {
+    // Check if the user has a session by looking at cookies in headers
+    const sessionCookie = request.cookies.get( "better-auth.session_token" );
 
-    if ( !session ) {
-        return NextResponse.redirect( new URL( '/sign-in', request.url ) );
+    if ( !sessionCookie ) {
+        return NextResponse.redirect( new URL( "/sign-in", request.url ) );
     }
-
-    // TODO make navigate function that directs you home if youre logged in and try to go sign in.
 
     // Continue normally if logged in.
     return NextResponse.next();
-}
-
-// Shield protects against the most common attacks from the OWASP top 10, before any page is called.
-const validate = aj
-    .withRule( shield( { mode: "LIVE" } ) )
-    .withRule( detectBot( { mode: "LIVE", allow: [ "CATEGORY:SEARCH_ENGINE", "G00G1E_CRAWLER" ] } ) );
-
-// This automatically turns on shield protection heheha.
-export default createMiddleware( validate );
+} );
 
 // This ensures that the middleware is not applied for the below.
 export const config = {
