faafbe686e377bbb287630495bb10863a6f97384
Ariadne <ariadnevk21@gmail.com>
2026-03-12 12:14:18 +1100

    lets try this again - basic

--- lib/actions/video.ts
+++ lib/actions/video.ts
@@ -286,7 +286,7 @@ export const generateClipImage = withErrorHandling( async ( videoId: string ) =>
         } )
         .from( videos )
         .innerJoin( user, eq( videos.userId, user.id ) )
-        .where( eq( videos.id, videoId ) )
+        .where( eq( videos.videoId, videoId ) )
 
     return result[0]
 } )
